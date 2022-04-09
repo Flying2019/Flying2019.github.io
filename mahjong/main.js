b03ca18d5c7f3c0530a5c9bbc2505af596a8f989
Flying2019 <815375530@qq.com>
2022-04-09 13:58:31 +0800

    Site updated: 2022-04-09 13:58:30

--- mahjong/main.js
+++ mahjong/main.js
@@ -1,16 +1,67 @@
 "use strict";
 let Three=4,Two=1,Max_value=9,Times=4,Typ=0,Extra=0;
-let Num=Three*3+Two*2-1;
-let a=new Array();
-const None="white",Choose="#88dd88",Red="#dd8888",Gray="#dddddd";
+let Num=Three*3+Two*2;
+let a=new Array(),answer=new Array();
+const None="white",Choose="#88dd88",Red="#dd8888",Gray="#dddddd",Blue="#9991ed",Yellow="yellow";
 const Back=8;
 const Space=32;
 const Tab=9;
 const Enter=13;
+const alpha=0.3;
+
+//随机一副牌
+
+function rand(x){return Math.floor(Math.random()*x);}
+
+function rand_hand()
+{
+    let ans=new Array(),c=new Array();
+    for(let i=1;i<=Max_value;i++) c[i]=0;
+    for(let i=0;i<Three;i++)
+    {
+        if(Math.random()<alpha)
+        {
+            let x=rand(Max_value)+1;
+            // console.log('pong '+x);
+            while(c[x]+3>Times) x=rand(Max_value)+1;
+            c[x]+=3;
+        }
+        else
+        {
+            let x=rand(Max_value-2)+1;
+            // console.log('shun '+x);
+            while(c[x]>=Times || c[x+1]>=Times || c[x+2]>=Times) x=rand(Max_value-2)+1;
+            c[x]++,c[x+1]++,c[x+2]++;
+        }
+    }
+    for(let i=0;i<Two;i++)
+    {
+        let x=rand(Max_value)+1;
+        while(c[x]+2>Times) x=rand(Max_value)+1;
+        c[x]+=2;
+    }
+    if(Extra)
+    {
+        let x=rand(Max_value)+1;
+        while(!c[x]) x=rand(Max_value)+1;
+        c[x]--;
+    }
+    for(let i=1;i<=Max_value;i++)
+        for(let k=0;k<c[i];k++) ans.push(i);
+    let res=new Array();
+    if(Extra)
+    {
+        Rong(res,ans);
+        for(let v of res) ans.push(v);
+    }
+    console.log(ans);
+    return ans;
+}
+
 
 //模板选择部分
 
-let pre=0;
+let pre=0,Win=false,Step;
 
 function start()
 {
@@ -19,13 +70,13 @@ function start()
     if(x=="ex"){alert("未完待续");return;}
     if(x==0)
     {
-        Three=4,Two=1,Max_value=9,Times=4,Typ=0,Extra=0;
+        Typ=0,Extra=0;
         restart();
         pre=x;
     }
     else if(x==1)
     {
-        Three=4,Two=1,Max_value=9,Times=4,Typ=0,Extra=1;
+        Typ=0,Extra=1;
         restart();
         pre=x;
     }
@@ -35,7 +86,7 @@ function start()
 
 function cmp(s,t){return parseInt(s)-parseInt(t);}
 
-function check(n=Num)
+function check(a,n=Num)
 {
     let cnt=new Array();
     for(let i=1;i<=Max_value;i++) cnt[i]=0;
@@ -70,7 +121,7 @@ function check(n=Num)
         for(let x2=0;x2<=Times;x2++)
         if(g[i][j][x1][x2])
         {
-            console.log(i+" "+j+" "+x1+" "+x2+" "+c);
+            // console.log(i+" "+j+" "+x1+" "+x2+" "+c);
             for(let tw=0;tw<=Times/2 && j+tw<=Two && tw*2<=c;tw++)
                 for(let ex=0;ex<=Times/3 && i+x1+ex<=Three && tw*2+ex*3<=c;ex++)
                 {
@@ -84,22 +135,32 @@ function check(n=Num)
     return f[Three][Two][0][0];
 }
 
-function Rong(ans)
+function Rong(ans,b=a)
 {
-    for(let i=0;i<Num;i++) if(a[i]<=0 || a[i]>Max_value) return false;
-    a.push(1);
+    for(let i=0;i<Num;i++) if(b[i]<=0 || b[i]>Max_value) return false;
+    b.push(1);
     for(let i=1;i<=Max_value;i++)
     {
-        a[Num]=i;
-        if(check(Num+1)) ans.push(i);
+        b[Num]=i;
+        if(check(b,Num+1)) ans.push(i);
     }
-    a.pop();
+    b.pop();
     if(ans.length==0) return false;
     return true;
 }
 
+function winning()
+{
+    bef+="<font color=red><p>You Win!</p><p id=\"step\"></p></font><input type=\"button\" class=\"gradual-button\" value=\"重试一次\" onclick=\"restart();\">";
+    document.getElementById('main').innerHTML=bef;
+    document.getElementById('step').innerHTML="Step: "+Step;
+    Win=true;
+}
+
 function End()
 {
+    let hav=new Array(),now=new Array(),vis=new Array();
+    for(let v=1;v<=Max_value;v++) vis[v]=0;
     if(Typ==0)
     {
         if(Extra)
@@ -107,23 +168,55 @@ function End()
             let rong=new Array();
             Rong(rong);
             if(rong.length==0) return false;
+            ++Step;
             let ans=new Array(),res=new Array();
             ans.push("Rong: "),res.push(None);
             for(let v=0;v<rong.length;v++)
                 ans.push(rong[v]),res.push(Red);
-            let str=generator(ans.length,ans,res);
-            bef+=generator()+str;
-            init();
+
+            for(let v=0;v<Num;v++) now.push(a[v]);
+            for(let v=0;v<rong.length;v++) now.push(rong[v]);
+            console.log(now);
+
+            let right=true;
+            for(let i=0;i<now.length;i++)
+                if(now[i]==answer[i]) hav[i]=1;
+                else hav[i]=0,right=false;
+            for(let i=0;i<answer.length;i++)
+                if(hav[i]==0) vis[answer[i]]++;
+            for(let i=0;i<now.length;i++)
+                if(hav[i]==0 && vis[now[i]]>0) vis[now[i]]--,hav[i]=2;
+            
+            for(let v=0;v<Num;v++) if(hav[v]==1) col[v]=Yellow;
+            else if(hav[v]==2) col[v]=Blue;
+            for(let v=0;v<rong.length;v++) if(hav[v+Num]==1) res[v+1]=Yellow;
+            else if(hav[v+Num]==2) res[v+1]=Blue;
+
+            let str=generator(ans.length,ans,res),u=generator();
+            bef+=u+str;
+            if(right) winning();
+            else init();
         }
         else
         {
-            let rong=check();
-            if(rong)
-            {
-                for(let i=0;i<Num;i++) col[i]=Red;
-                bef+=generator();
-                init();
-            }
+            let rong=check(a);
+            if(!rong) return;
+            ++Step;
+            let right=true;
+            for(let i=0;i<Num;i++) col[i]=Gray;
+            for(let i=0;i<Num;i++)
+                if(a[i]==answer[i]) hav[i]=1;
+                else hav[i]=0,right=false;
+            for(let i=0;i<Num;i++)
+                if(hav[i]==0) vis[answer[i]]++;
+            for(let i=0;i<Num;i++)
+                if(hav[i]==0 && vis[a[i]]>0) vis[a[i]]--,hav[i]=2;
+            for(let v=0;v<Num;v++) if(hav[v]==1) col[v]=Yellow;
+            else if(hav[v]==2) col[v]=Blue;
+            console.log(hav);
+            bef+=generator();
+            if(right) winning();
+            else init();
         }
     }
 }
@@ -138,7 +231,7 @@ function generator(n=Num,la=a,cl=col)
     // str+="</tr></table>"
     let str="<div border=\"1\" cellspacing=\"10\" style=\"margin-bottom: 30px\"><tr>";
     for(let i=0;i<n;i++)
-        str+="<font id=\"a"+i+"\" style=\"background-color: "+cl[i]+"; padding:12px; border-down: 10px; font-size:30px\">"+(la[i]==0?"*":la[i])+"</font>";
+        str+="<font id=\"a"+i+"\" style=\"background-color: "+cl[i]+"; padding:12px; font-size:30px; font-family: consola\">"+(la[i]==0?"*":la[i])+"</font>";
     str+="</div>"
     return str;
 }
@@ -151,17 +244,21 @@ let opt=new Array();
 let now_choose=0;
 function restart()
 {
+    Win=false;
     bef="";
+    Step=0;
     col=new Array();
     opt=new Array();
     now_choose=0;
     Num=Three*3+Two*2-Extra;
     a=new Array();
     init();
+    answer=rand_hand();
 }
 
 function load()
 {
+    if(Win) return;
     let str=bef+generator();
     document.getElementById('main').innerHTML=str;
 }
